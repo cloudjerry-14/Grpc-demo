b0a6b6c9c3daba373c60b9a0446a1ab4e4089622
Ashutosh Parihar <71233890+cloudjerry-14@users.noreply.github.com>
2025-03-10 02:09:19 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,40 +1,41 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net"
-	"net/http"
+    "fmt"
+    "log"
+    "net"
+    "net/http"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/health"
-	"google.golang.org/grpc/health/grpc_health_v1"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/health"
+    "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
-	// Start HTTP server
-	go func() {
-		http.HandleFunc("/", handleHTTP)
-		http.HandleFunc("/health", handleHTTPHealth)
-		log.Fatal(http.ListenAndServe(":8080", nil))
-	}()
+    // Start HTTP server
+    go func() {
+        http.HandleFunc("/", handleHTTP)
+        http.HandleFunc("/health", handleHTTPHealth)
+        log.Println("Starting HTTP server on :8080")
+        log.Fatal(http.ListenAndServe(":8080", nil))
+    }()
 
-	// Start gRPC server
-	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
-	s := grpc.NewServer()
-	grpc_health_v1.RegisterHealthServer(s, health.NewServer())
-	log.Fatal(s.Serve(lis))
+    // Start gRPC server
+    lis, err := net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalf("Failed to listen: %v", err)
+    }
+    s := grpc.NewServer()
+    grpc_health_v1.RegisterHealthServer(s, health.NewServer())
+    log.Println("Starting gRPC server on :50051")
+    log.Fatal(s.Serve(lis))
 }
 
 func handleHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from HTTP!")
+    fmt.Fprintf(w, "Hello from HTTP!")
 }
 
 func handleHTTPHealth(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "OK")
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "OK")
 }
